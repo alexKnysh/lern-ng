7ce8f6e6f85da9dc1655e21d8a755ccd93ebcb2d
aknysh <aknysh@aknysh.aknysh>
2019-01-07 23:30:53 +0300

    Atom settings and refactoring code.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var data = [
-    { name: "Test 1", status: 'No' },
-    { name: "Test 2", status: 'Yes' },
-    { name: "Test 3", status: 'No' },
-    { name: "Test 4", status: 'Yes' },
-    { name: "Test 5", status: 'Yes' },
-    { name: "Test 6", status: 'No' },
-    { name: "Test 7", status: 'Yes' }
+    { name: 'Test 1', status: 'No' },
+    { name: 'Test 2', status: 'Yes' },
+    { name: 'Test 3', status: 'No' },
+    { name: 'Test 4', status: 'Yes' },
+    { name: 'Test 5', status: 'Yes' },
+    { name: 'Test 6', status: 'No' },
+    { name: 'Test 7', status: 'Yes' },
 ]
 
-angular.module("myApp", [])
+angular
+    .module('myApp', [])
     .controller('myCtrl', function function_name($scope) {
-
-        $scope.lessonList = data;
+        $scope.lessonList = data
 
         $scope.onAdd = function() {
             $scope.lessonList.push({ name: $scope.lessonName, status: 'No' })
-            $scope.lessonName = '';
+
+            $scope.lessonName = ''
         }
 
         $scope.onRemove = function(index) {
-            $scope.lessonList.splice(index, 1);
+            $scope.lessonList.splice(index, 1)
         }
-
-    });
+    })
